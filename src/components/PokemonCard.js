f95970eef0dc74255000b13488467658d121ac0d
BrunoPierca <pierca18@gmail.com>
2024-06-04 16:23:15 -0300

    Card mockup

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -6,9 +6,13 @@ import {
   Badge,
   AspectRatio,
   Skeleton,
+  Avatar,
+  Tooltip,
+  Flex
 } from "@chakra-ui/react";
 import axios from "axios";
 import useSWR from "swr";
+import { FaBookmark } from "react-icons/fa";
 
 const fetchOnePokemon = async (id) => {
   try {
@@ -20,42 +24,79 @@ const fetchOnePokemon = async (id) => {
   }
 }
 
+
+const pokemonTypeColors = [
+  { name: "steel", value: "#BEBED7" },
+  { name: "water", value: "#6D96F7" },
+  { name: "bug", value: "#AEBE24" },
+  { name: "dragon", value: "#753CFF" },
+  { name: "electric", value: "#FFD734" },
+  { name: "ghost", value: "#755D9E" },
+  { name: "fire", value: "#F75534" },
+  { name: "ice", value: "#9EDFDF" },
+  { name: "fighting", value: "#96342C" },
+  { name: "normal", value: "#AEAE7D" },
+  { name: "grass", value: "#7DCF55" },
+  { name: "psychic", value: "#FF5D8E" },
+  { name: "rock", value: "#BEA63C" },
+  { name: "dark", value: "#968E8E" },
+  { name: "ground", value: "#E7C76D" },
+  { name: "poison", value: "#A645A6" },
+  { name: "flying", value: "#AE96F7" },
+  { name: "fairy", value: "#FFAAFF" },
+  { name: "?", value: "" },
+]
+
 export default function PokemonCard({ pokemon, handleViewPokemon }) {
   const { data: pokemonData, isLoading } = useSWR(`pokemon-${pokemon.name}}`, () => fetchOnePokemon(pokemon.url));
-  if (isLoading) return <Skeleton />
+  if (isLoading) return
   return (
-    <Stack
-      as="button"
-      spacing="5"
-      boxShadow="xl"
-      p="5"
-      w="full"
-      borderRadius="xl"
-      alignItems="center"
-      onClick={() => handleViewPokemon({ ...pokemon, ...pokemonData })}
-    >
-      <AspectRatio w="full" ratio={1}>
-        <Skeleton isLoaded={!isLoading}>
+    <Skeleton isLoaded={pokemonData} >
+      <Stack
+        as="button"
+        spacing="5"
+        boxShadow="xl"
+        p="5"
+        w="full"
+        borderRadius="xl"
+        alignItems="center"
+        onClick={() => handleViewPokemon({ ...pokemon, ...pokemonData })}
+        position={"relative"}
+      >
+        <Tooltip label="Saved">
+          <Flex justifyContent={"center"} alignItems={"center"} w={"24px"} h={"24px"} position={"absolute"} top={2} left={2} >
+            <FaBookmark />
+          </Flex>
+        </Tooltip>
+        <Tooltip label="Catched">
+          <Avatar position={"absolute"} size={"xs"} src="/logo.png" top={2} right={2} />
+        </Tooltip>
+        <AspectRatio w="full" ratio={1}>
           <Image
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonData.id}.png`}
             alt={`Picture of a ${pokemon.name}`}
             width={"100%"}
             height={"100%"}
           />
+        </AspectRatio>
+        <Text textAlign="center" textTransform="Capitalize">
+          {pokemon.name}
+        </Text>
+        <Skeleton isLoaded={!isLoading} >
+          <HStack gap={2}>
+            {pokemonData.types.map((type) => {
+              return (
+                <Badge size="xs" key={type.slot}
+                  sx={{
+                    backgroundColor: pokemonTypeColors.find((color) => color.name === type.type.name)?.value ?? pokemonTypeColors[-1]
+                  }}>
+                  {type.type.name}
+                </Badge>
+              )
+            })}
+          </HStack>
         </Skeleton>
-      </AspectRatio>
-      <Text textAlign="center" textTransform="Capitalize">
-        {pokemon.name}
-      </Text>
-      <Skeleton isLoaded={!isLoading} >
-        <HStack gap={2}>
-          {pokemonData.types.map((type) => (
-            <Badge size="xs" key={type.slot}>
-              {type.type.name}
-            </Badge>
-          ))}
-        </HStack>
-      </Skeleton>
-    </Stack>
+      </Stack>
+    </Skeleton>
   );
 }
